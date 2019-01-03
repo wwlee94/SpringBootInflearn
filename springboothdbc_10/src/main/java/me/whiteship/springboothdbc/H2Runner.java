369019fe39cc5443b1625a253ebce389ebd5a8bb
wwlee94 <38131683+wwlee94@users.noreply.github.com>
2019-01-03 20:31:50 +0900

    -스프링 데이터 3,4부 MySQL, PostgreSQL

--- springboothdbc_10/src/main/java/me/whiteship/springboothdbc/H2Runner.java
+++ springboothdbc_10/src/main/java/me/whiteship/springboothdbc/H2Runner.java
@@ -10,6 +10,19 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.Statement;
 
+//H2SQL + MYSQL
+/*
+    MYSQL으로 사용하려면 해당 설정 해야함
+    spring.datasource.url=jdbc:mysql://localhost:3306/springboot?useSSL=false&characterEncoding=UTF-8&serverTimezone=UTC
+    spring.datasource.username=root
+    spring.datasource.password=gjlee7257
+ */
+/*
+    POSTGRES으로 사용하려면 해당 설정 (docker로 만듬)
+    spring.datasource.url=jdbc:postgresql://localhost:5432/springboot
+    spring.datasource.username=wwlee94
+    spring.datasource.password=wwlee94
+ */
 @Component
 public class H2Runner implements ApplicationRunner {
 
@@ -23,14 +36,16 @@ public class H2Runner implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         try(Connection connection = dataSource.getConnection()) {
 
+            System.out.println(dataSource.getConnection());
+            System.out.println(connection.getMetaData().getDriverName());
             System.out.println(connection.getMetaData().getURL());
             System.out.println(connection.getMetaData().getUserName());
 
             Statement statement = connection.createStatement();
-            String sql = "CREATE TABLE USER(ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (ID))";
+            String sql = "CREATE TABLE account(ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (ID))";
             statement.executeUpdate(sql);
         }
 
-        jdbcTemplate.execute("INSERT INTO USER VALUES (1,'keesun')");
+        jdbcTemplate.execute("INSERT INTO account VALUES (2,'woowon')");
     }
 }
